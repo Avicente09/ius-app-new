d8c229edf395cffd5b963850ad94fddf1dc4f5e6
shirmigod <shirmigod@gmail.com>
2023-02-15 12:31:54 -0600

    Fix: lint-staged config file fixes to run eslint and prettier on precommit hook (#7)

--- lint-staged.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    '*.{js,jsx,ts,tsx}': [
-        'eslint --max-warnings=0', 
-        'jest',
-        () => 'tsc-files --noEmit',
-    ],
-    '*.{js,jsx,ts,tsx,json,css,js}': ['prettier --write'],
-}
